40a414195ac1de635f08bafe47eda37c45b17ae9
c04o <c4ld@proton.me>
2025-04-02 07:47:53 -0600

    Ëjercicio 8. Ordenar alfabeticamente cadenas

---
+++ exercise8.py
@@ -0,0 +1,9 @@
+"""
+8. Escribe un programa que recorra una lista de cadenas y las ordene
+alfabéticamente en orden ascendente.
+"""
+
+l = ["Hola,", "mundo!", "Python", "es", "genial"]
+
+r = sorted(l)
+print(r)
