a5b99032426b99a0cad32200f70ef393e2b696af
c04o <c4ld@proton.me>
2025-04-02 07:31:53 -0600

    Ejercicio 5. Reemplazar caracter

---
+++ exercise5.py
@@ -0,0 +1,12 @@
+"""
+5. Escribe un programa que recorra una lista de cadenas y reemplace todas
+las apariciones de un carácter específico con otro carácter, luego imprime la
+lista modificada.
+"""
+
+l = ["Hola", "mundo", "Python", "es", "genial"]
+c_viejo = "o"
+c_nuevo = "x"
+
+r = [c.replace(c_viejo, c_nuevo) for c in l]
+print(r)
